321a853c66de94e910dac854d3f42c5aaf4570f2
Yeh Dil Mange More <48553225+chitwang12@users.noreply.github.com>
2023-11-29 12:32:34 +0000

    scopes.js

---
+++ scopes.js
@@ -0,0 +1,18 @@
+let a = 10;
+const b = 20;
+var c = 30;
+
+if(true){
+    let a = 10;
+    const b = 20;
+    var c = 30;
+
+}
+
+// {} these are known as scopes , all the accessing problem for a particaulr variable starts over here
+
+
+
+console.log(a);
+console.log(b);
+console.log(c);
